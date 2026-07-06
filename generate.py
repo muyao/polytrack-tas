d6ba07ea992e70f01e76ccc1f882c1b87a55522e
muyao <55979740+muyao@users.noreply.github.com>
2026-07-06 20:41:33 +0200

    v1.0.0

--- generate.py
+++ generate.py
@@ -1,11 +1,104 @@
-import zlib
 import base64
+import json
+import os
+import secrets
+import subprocess
+import zlib
+from pathlib import Path
 
-raw_bytes = b"\x02\x00\x00\xff\x01\x00\xff\xff\xff\x02\x00\x00\xef\x05\x00\x57\x00\x00\x04\x00\x00\x00\x00\x00\x41\x00\x00\xa6\x01\x00\x34\x00\x00\x02\x00\x00\x21\x09\x00\x3e\x00\x00\x00\x00\x00"
+SH_PATH = "/bin/bash"
+USER_AGENT = "Mozilla/5.0"
+NICKNAME = "XxXxxxXxX"
+CAR_STYLE = "AAAAAC13Fv___xMTE2ZmZg"
+TRACK_ID = "95e4bf5c2a76b49ad1ce0f7fd9d6ca777a09aaf6492f9a7415afde3427a8ac66"
+FRAMES = "3282"
+ONLY_VERIFIED = "false"
 
-compressed = zlib.compress(raw_bytes, level=9)
+# Load instructions
+with open(Path(__file__).resolve().parent / "instructions.json") as f:
+	instructions = json.load(f)
+
+# Temporary storage
+wasd = {
+	"w": [],
+	"a": [],
+	"s": [],
+	"d": []
+}
+
+# Recording. Will get appended from wasd in order W, A, S, D
+rec = b""
+
+# Iterate through each frame (Frame as in one screen refresh)
+for frame in instructions:
+	instruction = instructions[frame]
+
+	# For each action of an instruction
+	for k in instruction:
+		d = instruction[k]
+
+		# If key is not WASD then throw error
+		if k not in wasd:
+			raise Exception(f"Unknown key: {k} in frame {frame}. See instructions.json.")
+
+		# Store instruction, convert to little-endian
+		wasd[k].append(int(frame).to_bytes(3, byteorder="little"))
+		wasd[k].append(int(d).to_bytes(3, byteorder="little"))
+
+# First add all W
+rec += len(wasd["w"]).to_bytes(3, byteorder="little")
+for v in wasd["w"]:
+	rec += v
+
+# Then all A
+rec += len(wasd["a"]).to_bytes(3, byteorder="little")
+for v in wasd["a"]:
+	rec += v
+
+# S
+rec += len(wasd["s"]).to_bytes(3, byteorder="little")
+for v in wasd["s"]:
+	rec += v
+
+# D
+rec += len(wasd["d"]).to_bytes(3, byteorder="little")
+for v in wasd["d"]:
+	rec += v
+
+# Add three null bytes at the end
+rec += b"\x00\x00\x00"
+
+# Compress with zlib
+rec = zlib.compress(rec, level=9)
 
 # Convert to Base64
-w14_base64 = base64.b64encode(compressed).decode('utf-8')
+rec = base64.b64encode(rec).decode('utf-8')
 
-print(w14_base64.replace("/", "_").replace("+", "-").replace("=", ""))
+# Polytrack uses - and _ instead of + and /
+rec = rec.replace("+", "-").replace("/", "_").replace("=", "")
+
+# Generate the shellscript
+with open("/tmp/polyhax.post.sh", "w") as f:
+	f.write(f"#!{SH_PATH}\n")
+	f.write("curl 'https://vps.kodub.com/v6/leaderboard' ")
+	f.write("-H 'Content-Type: application/x-www-form-urlencoded' ")
+	f.write("-H 'Origin: https://app-polytrack.kodub.com' ")
+	f.write("-H 'Referer: https://app-polytrack.kodub.com/' ")
+	f.write(f"-H 'User-Agent: {USER_AGENT}' ")
+	f.write("--data-raw 'version=0.6.2&")
+	f.write(f"userToken={secrets.token_hex(32)}&")
+	f.write(f"nickname={NICKNAME}&")
+	f.write(f"carStyle={CAR_STYLE}&")
+	f.write(f"trackId={TRACK_ID}&")
+	f.write(f"frames={FRAMES}&")
+	f.write(f"recording={rec}&")
+	f.write(f"onlyVerified={ONLY_VERIFIED}'")
+
+# Give file execution permissions
+os.chmod("/tmp/polyhax.post.sh", 0o744)
+
+# Execute shellscript
+subprocess.run(["/tmp/polyhax.post.sh"])
+
+# Clean up
+os.remove("/tmp/polyhax.post.sh")
