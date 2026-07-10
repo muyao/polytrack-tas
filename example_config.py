875a472810b1f487b28d2a9c57649c2082f86b4c
muyao <55979740+muyao@users.noreply.github.com>
2026-07-10 19:09:44 +0200

    Add examples for v2

---
+++ example_config.py
@@ -0,0 +1,26 @@
+# Name that will show up in leaderboards
+NICKNAME = "TAS"
+
+# Track id
+TRACK_ID = ""
+
+# Length of the recording. 1000 frames is one second
+FRAMES = "60000"
+
+# Car style
+CAR_STYLE = "AAUAAAAAAAAAAP8A_wAAAA"
+
+# File name of the instructions json
+INSTRUCTIONS_NAME = "instructions.json"
+
+# Recording to decode using decode.py
+DECODE_RECORDING = "eNpNkEtKA0EYhL-R0YySSLYuIlE0mWRMfCbq-EADgpHgIw-ykIggiAsFF25cCYGsxI1r75ELeAUv4A28gX9XEGyaprq7_qr6_wRuXeYZjvHp0_D49rmHLkRQgg2BA8hBDCHs6RzhTViCLZiFbSjodw529PICC7AP67Co32WY19U45xlepf84w1vSSVXlmIWKylckHqtwFcrahvNSKImfE8dwUXmif9eiwOgllHX0t03hSy5NOIIzqKtlAzVowwBuZTcNH2k6kxynwONGwaYmeLf80rkYJ2OcgETShRkELqHR1qAHu0bQTNqaUlemPWXoqMFridxpUH3PSf2kXF8PmuEzHMITtKABVxrmqQQNnEjN1i8JXieW"
+
+# User agent for requests
+USER_AGENT = "Mozilla/5.0"
+
+# Only verified
+ONLY_VERIFIED = "false"
+
+# Polytrack version
+POLYTRACK_VERSION = "0.6.2"
