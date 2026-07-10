003174bc75f4b136d426e8468d88995c7f46a291
muyao <55979740+muyao@users.noreply.github.com>
2026-07-10 17:01:40 +0200

    decode.py now converts b64 recording to json

--- decode.py
+++ decode.py
@@ -1,35 +1,199 @@
-import zlib
 import base64
+import json
+import zlib
+from config import *
+from pathlib import Path
 
-w1_b64 = "eNpjYgABCSYGZAAAAcMAHQ=="
-sw_b64 = "eNpjYmDgYmV4zMwABEwggiGGkQECABfxAVc="
-ins_b64 = "eNpjZEAHAAAkAAI="
-allc_b64 = "eNpjYmD4yM2wgYGBiYGhmplhMpgBBElgxg82hnYwFwBn5wSt"
-allc2_b64 = "eNpjYmD4zMuQyczAxMDwnpUhnIGBhQEEHBkYljEymDCAxBU5GezAggB8WwRG"
+if __name__ != "__main__":
+	exit(0)
 
-w10_b64 = "eNoTYQABIwaGWQwMSgwMRxgYVBgYKhgYrBgY+sFkL1i2jYFBD6xGlYFhIgODEwPDEgYGUwYUAAAytAdE"
-w11_b64 = "eNpVyqENgDAUANE3RZNugMBQJAmYzkRQIFDdoOMSvuuJy4lLfjIvC42dk5mHGl65meiUeDYujuhi4ANLswdo" 
-w12_b64 = "eNpNxiEKgEAUQME5xlZhEWxmy2LwTms3KRhEtnhZ8SdfGF7yldkorExcdJz0NBYqI3d4MLMz8MTX8N8LlD0IdQ=="
-w14_b64 = "eNpVyjEOQFAUBdGzBq1fiKBQKCwSC/ilTqmxRM/rTDG5mdziY2Kjp1LYGdJr9ig3LWfucMPFmJ+FIx2942H24wUA8QmW"
+# Add padding
+rec = DECODE_RECORDING + "=" * (-len(DECODE_RECORDING) % 4)
 
-w1 = zlib.decompress(base64.b64decode(w1_b64))
-sw = zlib.decompress(base64.b64decode(sw_b64))
-ins = zlib.decompress(base64.b64decode(ins_b64))
-allc = zlib.decompress(base64.b64decode(allc_b64))
-allc2 = zlib.decompress(base64.b64decode(allc2_b64))
+# Decode base64
+rec = base64.urlsafe_b64decode(rec)
 
-w10 = zlib.decompress(base64.b64decode(w10_b64))
-w11 = zlib.decompress(base64.b64decode(w11_b64))
-w12 = zlib.decompress(base64.b64decode(w12_b64))
-w14 = zlib.decompress(base64.b64decode(w14_b64))
+# Decompress with zlib
+rec = zlib.decompress(rec)
 
-print("1 Tap Uncompressed:", w1.hex(" "))
-print("Sw Uncompressed:", sw.hex(" "))
-print("Ins Uncompressed:", ins.hex(" "))
-print("Allc Uncompressed:", allc.hex(" "))
-print("Allc2 Uncompressed:", allc2.hex(" "))
+# Turn rec into a list. 3 bytes per group
+rec = [int.from_bytes(rec[i : i + 3], byteorder="little") for i in range(0, len(rec), 3)]
+print(rec)
 
-print("10 Taps Uncompressed:", w10.hex(" "))
-print("11 Taps Uncompressed:", w11.hex(" "))
-print("12 Taps Uncompressed:", w12.hex(" "))
-print("14 Taps Uncompressed:", w14.hex(" "))
+# Will be final output
+output = {}
+
+# For turning delta time into absolute frames
+elapsed = 0
+
+# W actions
+w_len = rec[0]
+w_actions = rec[1 : w_len + 1]
+
+# Add W actions to output
+for idx, val in enumerate(w_actions):
+
+	elapsed += val
+
+	# Idx is even: starting frame
+	if idx % 2 == 0:
+		frame = elapsed
+
+		# If frame does not exist, add frame
+		if frame not in output:
+			output[frame] = {}
+
+		# For infinite keydown
+		if idx == len(w_actions) - 1 and len(w_actions) % 2 != 0:
+			output[frame]["w"] = -1
+			continue
+
+		# Will be changed in next iteration
+		output[frame]["w"] = None
+
+		# Skip
+		continue
+	
+	# Duration, if index is odd
+	output[frame]["w"] = val
+
+# Reset elapsed
+elapsed = 0
+
+# D actions
+d_len = rec[w_len + 1]
+d_actions = rec[w_len + 2 : w_len + d_len + 2]
+
+# Add D actions to output
+for idx, val in enumerate(d_actions):
+
+	elapsed += val
+
+	# Idx is even: starting frame
+	if idx % 2 == 0:
+		frame = elapsed
+
+		# If frame does not exist, add frame
+		if frame not in output:
+			output[frame] = {}
+
+		# For infinite keydown
+		if idx == len(d_actions) - 1 and len(d_actions) % 2 != 0:
+			output[frame]["d"] = -1
+			continue
+
+		# Will be changed in next iteration
+		output[frame]["d"] = None
+
+		# Skip
+		continue
+	
+	# Duration, if index is odd
+	output[frame]["d"] = val
+
+# Reset elapsed
+elapsed = 0
+
+# S actions
+s_len = rec[w_len + d_len + 2]
+s_actions = rec[w_len + d_len + 3 : w_len + d_len + s_len + 3]
+
+# Add S actions to output
+for idx, val in enumerate(s_actions):
+
+	elapsed += val
+
+	# Idx is even: starting frame
+	if idx % 2 == 0:
+		frame = elapsed
+
+		# If frame does not exist, add frame
+		if frame not in output:
+			output[frame] = {}
+
+		# For infinite keydown
+		if idx == len(s_actions) - 1 and len(s_actions) % 2 != 0:
+			output[frame]["s"] = -1
+			continue
+
+		# Will be changed in next iteration
+		output[frame]["s"] = None
+
+		# Skip
+		continue
+	
+	# Duration, if index is odd
+	output[frame]["s"] = val
+
+# Reset elapsed
+elapsed = 0
+
+# A actions
+a_len = rec[w_len + d_len + s_len + 3]
+a_actions = rec[w_len + d_len + s_len + 4 : w_len + d_len + s_len + a_len + 4]
+
+# Add A actions to output
+for idx, val in enumerate(a_actions):
+
+	elapsed += val
+
+	# Idx is even: starting frame
+	if idx % 2 == 0:
+		frame = elapsed
+
+		# If frame does not exist, add frame
+		if frame not in output:
+			output[frame] = {}
+
+		# For infinite keydown
+		if idx == len(a_actions) - 1 and len(a_actions) % 2 != 0:
+			output[frame]["a"] = -1
+			continue
+
+		# Will be changed in next iteration
+		output[frame]["a"] = None
+
+		# Skip
+		continue
+	
+	# Duration, if index is odd
+	output[frame]["a"] = val
+
+# Reset elapsed
+elapsed = 0
+
+# Reset actions
+reset_len = rec[w_len + d_len + s_len + a_len + 4]
+reset_actions = rec[w_len + d_len + s_len + a_len + 5 : w_len + d_len + s_len + a_len + reset_len + 5]
+
+# Add reset actions to output
+for idx, val in enumerate(reset_actions):
+
+	elapsed += val
+
+	# Idx is even: starting frame
+	if idx % 2 == 0:
+		frame = elapsed
+
+		# If frame does not exist, add frame
+		if frame not in output:
+			output[frame] = {}
+
+		# For infinite keydown
+		if idx == len(reset_actions) - 1 and len(reset_actions) % 2 != 0:
+			output[frame]["reset"] = -1
+			continue
+
+		# Will be changed in next iteration
+		output[frame]["reset"] = None
+
+		# Skip
+		continue
+	
+	# Duration, if index is odd
+	output[frame]["reset"] = val
+
+output = {k: output[k] for k in sorted(output, key=int)}
+
+with open(Path(__file__).resolve().parent / INSTRUCTIONS_NAME, "w") as f:
+	json.dump(output, f, indent="\t")
