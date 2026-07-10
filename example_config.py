4fabc797e796917936926e491fd576e6815944c5
muyao <55979740+muyao@users.noreply.github.com>
2026-07-10 14:54:19 +0200

    Add examples

---
+++ example_config.py
@@ -0,0 +1,23 @@
+# Name that will show up in leaderboards
+NICKNAME = "TAS"
+
+# Track id
+TRACK_ID = ""
+
+# Length of the recording. 1000 frames is one second
+FRAMES = "60000"
+
+# Car style
+CAR_STYLE = ""
+
+# File name of the instructions json
+INSTRUCTIONS_NAME = "instructions.json"
+
+# User agent for requests
+USER_AGENT = "Mozilla/5.0"
+
+# Only verified
+ONLY_VERIFIED = "false"
+
+# Polytrack version
+POLYTRACK_VERSION = "0.6.2"
