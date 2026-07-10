c8673c47d9ffd1cf7877f54e7543cb95dd6386c0
muyao <55979740+muyao@users.noreply.github.com>
2026-07-10 14:33:15 +0200

    Make config.py more readable

--- config.py
+++ config.py
@@ -1,9 +1,23 @@
-INSTRUCTIONS_NAME = "instructions.json"
-POLYTRACK_VERSION = "0.6.2"
-SH_PATH = "/bin/bash"
-USER_AGENT = "Mozilla/5.0"
+# Name that will show up in leaderboards
 NICKNAME = "TAS"
-CAR_STYLE = "AAAAAC13Fv___xMTE2ZmZg"
+
+# Track id
 TRACK_ID = "c45242344aa1b592a13a56c872f747a6df32bdc106a23b4f494673a66b33934a"
+
+# Length of the recording. 1000 frames is one second
 FRAMES = "60000"
+
+# Car style
+CAR_STYLE = "AAAAAC13Fv___xMTE2ZmZg"
+
+# File name of the instructions json
+INSTRUCTIONS_NAME = "instructions.json"
+
+# User agent for requests
+USER_AGENT = "Mozilla/5.0"
+
+# Only verified
 ONLY_VERIFIED = "false"
+
+# Polytrack version
+POLYTRACK_VERSION = "0.6.2"
